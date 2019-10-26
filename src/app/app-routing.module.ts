b2dbdf78ad9482f1e07ccce21f7b3c64004f8656
Brandon In <brandonin@me.com>
2019-10-25 18:11:35 -0700

    added exchange rates and graphql query

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component'
+import { ExchangeratesComponent } from './exchangerates/exchangerates.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+  },
+  {
+    path: 'exchangerates',
+    component: ExchangeratesComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ExchangeratesComponent } from './exchangerates/exchangerates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    ExchangeratesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
